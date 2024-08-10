d9d4859b1899f81d391fb7d1d661d94bc73fb403
hamzah-syed <hamzah.syed17@gmail.com>
2024-08-10 16:49:35 +0500

    updated ci env

--- scripts/docker-compose-up.sh
+++ scripts/docker-compose-up.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Handle the .env.common file
-if [ ! -f ".env.common" ]; then
-    if [ "$CI" == "true" ]; then
-        echo "Creating temporary .env.common file for CI environment"
-        cat <<EOT >> .env.common
-# Temporary .env.common for CI
-KAFKA_BOOTSTRAP_SERVERS=kafka:9092
-EOT
-    else
-        echo ".env.common file not found! Please create it in the project root."
-        exit 1
-    fi
-fi
-
 # Check if running in CI environment
 if [ "$CI" == "true" ]; then
     echo "Running in CI environment"
-    export ENV_FILE=.env.example
+    export ENV_FILE=".env.example"
 else
     # Check if ENVIRONMENT variable is set
     if [ -z "$ENVIRONMENT" ]; then
@@ -27,10 +13,10 @@ else
 
     # Set the appropriate ENV_FILE based on ENVIRONMENT
     if [ "$ENVIRONMENT" == "development" ]; then
-        export ENV_FILE=.env.dev
+        export ENV_FILE=".env.dev"
         echo "Using Development Environment"
     elif [ "$ENVIRONMENT" == "production" ]; then
-        export ENV_FILE=.env.prod
+        export ENV_FILE=".env.prod"
         echo "Using Production Environment"
     else
         echo "Invalid ENVIRONMENT value. Exiting."
@@ -38,11 +24,11 @@ else
     fi
 fi
 
+# Ensure that the ENV_FILE is not pointing to a directory
+if [ -d "$ENV_FILE" ]; then
+    echo "Error: $ENV_FILE is a directory, but it should be a file."
+    exit 1
+fi
+
 # Run docker-compose with the selected environment file
 docker-compose up -d --build
-
-# Clean up temporary .env.common if created
-if [ "$CI" == "true" ] && [ -f ".env.common" ]; then
-    echo "Cleaning up temporary .env.common file"
-    rm .env.common
-fi
